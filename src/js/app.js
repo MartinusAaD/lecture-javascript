5035d0ba26d532fb923432e9a5eb2659c144d627
Martinus Aamot Dahl <maraad97@gmail.com>
2024-10-09 12:42:23 +0200

    added js content

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+console.log("First JavaScript message ")
+
+let country;
+country = "Finland";
+console.log(country);
+
+var anotherCountry = "Sweden";
+console.log(anotherCountry);
+
+const weekDay ="Monday";
+console.log(weekDay);
