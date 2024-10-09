bba7593c9c92bc6644bb9fe5d2bbc996aca96fab
Martinus Aamot Dahl <maraad97@gmail.com>
2024-10-09 13:18:20 +0200

    Added more javascript content, primitives

--- src/js/app.js
+++ src/js/app.js
@@ -7,5 +7,32 @@ console.log(country);
 var anotherCountry = "Sweden";
 console.log(anotherCountry);
 
-const weekDay ="Monday";
+const weekDay = "Monday";
 console.log(weekDay);
+
+
+
+let country = "Norway";
+let studentNumber = 12345;
+let studentstatus = false;
+let grade;
+let classRoom = null;
+let fruitList = ["Watermelon", "Apple", "Orange"]
+let person = {
+    name: "Jonas",
+    age: 60,
+    city: "Las Vegas"
+}
+
+function add(){
+    return 1 + 1;
+}
+
+console.log(typeof country);
+console.log(typeof studentNumber);
+console.log(typeof studentStatus);
+console.log(typeof grade);
+console.log(typeof classRoom);
+
+console.log(typeof fruitList);
+console.log(typeof person);
